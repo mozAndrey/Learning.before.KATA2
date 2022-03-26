b4f9c320e06ae9c63393e77b4cffd5f0d845de5c
mozAndrey <1852721@mail.ru>
2022-03-26 17:54:43 +0300

    Исправил синтаксическую ошибку при введении средней оценки

---
+++ src/ArrayListTests/Main.java
@@ -0,0 +1,45 @@
+package ArrayListTests;
+
+import java.util.*;
+
+public class Main {
+    public Main() {
+    }
+
+
+    public static void main(String[] args) {
+        Car c1 = new Car("Red", 4);
+        Car c2 = new Car("Pink", 4);
+        Car c3 = new Car("Green", 20);
+        Car c4 = new Car("Red", 4);
+        System.out.println(c1.compareTo(c2));
+        System.out.println(c1.equals(c2));
+//        System.out.println(c1.color.compareTo(c2.color));
+//        ArrayList<Car> arrOfCars = new ArrayList<>();
+//        arrOfCars.add(c1);
+//        arrOfCars.add(c2);
+//        arrOfCars.add(c3);
+//        ArrayList<Integer> arrOfInts = new ArrayList<>();
+//        for (int i = 0; i < arrOfCars.size(); i++) {
+//            arrOfInts.add(i);
+//        }
+//        Map<Car, Integer> mapOfCars = new HashMap<>();
+//        for (int i = 0; i < arrOfCars.size(); i++) {
+//            mapOfCars.put(arrOfCars.get(i), arrOfInts.get(i));
+//        }
+//        ArrayList<Car> arrayListOfCars = new ArrayList<>();
+//        ArrayList<Integer> arrayListOfIntegers = new ArrayList<>();
+//        boolean isContains = mapOfCars.containsKey(c4);
+//        System.out.println("Map contains c4 before editing = " + isContains);
+//        c4.setColor("Yellow");
+//        c4.setCountOfDoors(6);
+//        System.out.println("Map contains c4 after editing = " + mapOfCars.containsKey(c4));
+//        c2.setColor("Yellow");
+//        c2.setCountOfDoors(6);
+//        System.out.println("Hash c2 & c4 = " + (c2.hashCode() == c4.hashCode()));
+//        System.out.println("Equals c2 & c4 = " + c2.equals(c4));
+//        System.out.println("Map contains c4 after editing c2 = " + mapOfCars.containsKey(c4));
+
+
+    }
+}
--- src/PriorityQueue/Main.java
+++ src/PriorityQueue/Main.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        Student st1 = new Student("Andrey", "Mozal",4,5.2,6.2,7.8);
-        Student st2 = new Student("Ekaterina", "Mozal", 3, 4.2,5.4,6.7);
-        Student st3 = new Student("Petr", "Ezhov",2,4.2,6.5,2.6);
-        Student st4 = new Student("Dedok", "Nesterov",1,3.2,6/5,7.6);
+        Student st1 = new Student("Andrey", "Mozal", 4, 5.2, 6.2, 7.8);
+        Student st2 = new Student("Ekaterina", "Mozal", 3, 4.2, 5.4, 6.7);
+        Student st3 = new Student("Petr", "Ezhov", 2, 4.2, 6.5, 2.6);
+        Student st4 = new Student("Dedok", "Nesterov", 1, 3.2, 6.5, 7.6);
         Student st5 = new Student("Kolya", "Nesterov");
         Student st6 = new Student("Petya", "Peskov");
-        List <Student> arrayListOfStudents = new ArrayList<>();
+        List<Student> arrayListOfStudents = new ArrayList<>();
         arrayListOfStudents.add(st1);
         arrayListOfStudents.add(st2);
         arrayListOfStudents.add(st3);
